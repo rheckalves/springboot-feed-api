1400b11a860e35a9fa17847d9512609fe2099f8e
Ricardo Heck alves <ricardoheckalves@icloud.com>
2021-06-25 03:44:12 -0300

    initial commit

---
+++ src/main/java/com/segware/javabackendtest/JavaBackendTestApplication.java
@@ -0,0 +1,13 @@
+package com.segware.javabackendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendTestApplication.class, args);
+	}
+
+}
